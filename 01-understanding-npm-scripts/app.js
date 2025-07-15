551f70445be945f635325c6cb221b7754c6966b5
KonradWycka24 <wycka.konrad24@gmail.com>
2025-07-15 18:23:26 +0200

    15.07.2025

--- 01-understanding-npm-scripts/app.js
+++ 01-understanding-npm-scripts/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
-
+const path = require('path');
 const adminRoutes = require('./routes/admin.js');
 const shopRoutes = require('./routes/shop.js');
 const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.urlencoded({extended:  false}));
-app.use(adminRoutes);
+app.use('/admin',adminRoutes);
 app.use(shopRoutes);
+app.use((req,res,next) => {
+    res.status(404).sendFile(path.join(__dirname,'views','not-found.html'));
 
+})
 app.listen(3000);
--- 01-understanding-npm-scripts/routes/admin.js
+++ 01-understanding-npm-scripts/routes/admin.js
@@ -1,10 +1,18 @@
+const path = require('path');
+const rootDir = require('../util/path');
 const express = require('express');
+
 const router = express.Router();
-router.use('/add-product',(req,res,next) => {
-    res.send('<form action ="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
-router.post('/product',(req,res) => {
-   console.log(req.body); 
-res.redirect('/');
+
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
 });
+
 module.exports = router;
