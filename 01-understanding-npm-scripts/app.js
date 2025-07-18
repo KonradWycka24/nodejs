928c4e0d86b0b53e6f9a5fdbe4a89af4bc67002e
KonradWycka24 <wycka.konrad24@gmail.com>
2025-07-18 19:01:21 +0200

    18.08.2025

--- 01-understanding-npm-scripts/app.js
+++ 01-understanding-npm-scripts/app.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const path = require('path');
-const adminRoutes = require('./routes/admin.js');
+const adminData = require('./routes/admin.js');
 const shopRoutes = require('./routes/shop.js');
 const bodyParser = require('body-parser');
 const app = express();
+app.set('view engine','pug');
 app.use(bodyParser.urlencoded({extended:  false}));
-app.use('/admin',adminRoutes);
+app.use(express.static(path.join(__dirname,'public')));
+app.use('/admin',adminData.routes);
 app.use(shopRoutes);
 app.use((req,res,next) => {
-    res.status(404).sendFile(path.join(__dirname,'views','not-found.html'));
+    res.status(404).render('404', {pageTitle: 'Page not Found'});
 
 })
 app.listen(3000);
